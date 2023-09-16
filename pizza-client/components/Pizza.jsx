0359aa89ba6ab098fbbeb6dd27f4798b447e7af9
cwjki <cwjki97@gmail.com>
2023-09-16 20:31:16 +0200

    simulate the fetching pizza data on the initial page request

--- pizza-client/components/Pizza.jsx
+++ pizza-client/components/Pizza.jsx
@@ -8,6 +8,33 @@ const Pizza = () => {
   const [data, setData] = useState([]);
   const [maxId, setMaxId] = useState(0);
 
+  useEffect(() => {
+    // Simulate fetching data from API
+    const fetchPizzaData = () => {
+      const pizzaData = [
+        {
+          id: 1,
+          name: "Margherita",
+          description: "Tomato sauce, mozzarella, and basil",
+        },
+        {
+          id: 2,
+          name: "Pepperoni",
+          description: "Tomato sauce, mozzarella, and pepperoni",
+        },
+        {
+          id: 1,
+          name: "Hawaiian",
+          description: "Tomato sauce, mozzarella, ham, and pinapple",
+        },
+      ];
+      setData(pizzaData);
+      setMaxId(Math.max(...pizzaData.map((pizza) => pizza.id)));
+    };
+
+    fetchPizzaData();
+  }, []);
+
   const handleCreate = (item) => {};
   const handleUpdate = (item) => {};
   const handleDelete = (id) => {};
