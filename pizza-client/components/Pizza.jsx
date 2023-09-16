94cb7b16da3ee3f52715c8a935ae22c0796c1b93
cwjki <cwjki97@gmail.com>
2023-09-16 20:23:55 +0200

    structure of Pizza Component

--- pizza-client/components/Pizza.jsx
+++ pizza-client/components/Pizza.jsx
@@ -2,13 +2,25 @@
 import PizzaList from "./PizzaList";
 import { useState, useEffect } from "react";
 
+const term = "Pizza";
+
 const Pizza = () => {
   const [data, setData] = useState([]);
   const [maxId, setMaxId] = useState(0);
 
+  const handleCreate = (item) => {};
+  const handleUpdate = (item) => {};
+  const handleDelete = (id) => {};
+
   return (
     <>
-      <PizzaList />
+      <PizzaList
+        name={term}
+        data={data}
+        onCreate={handleCreate}
+        onUpdate={handleUpdate}
+        onDelete={handleDelete}
+      />
     </>
   );
 };
